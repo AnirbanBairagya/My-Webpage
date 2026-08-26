fa0e30525d7b582298aad88a7cc823f432dd579e
Anirban Bairagya <sonalibairagya139@gmail.com>
2026-08-26 19:56:03 +0530

    Add Blogs component to App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Hero from './components/Hero.jsx'
 import About from './components/About.jsx'
 import Skills from './components/Skills.jsx'
 import Projects from './components/Projects.jsx'
+import Blogs from './components/Blogs.jsx'
 import { Experience, Education } from './components/ExperienceEducation.jsx'
 import Interests from './components/Interests.jsx'
 import Contact, { Footer } from './components/Contact.jsx'
@@ -15,6 +16,7 @@ export default function App() {
       <About />
       <Skills />
       <Projects />
+      <Blogs />
       <Experience />
       <Education />
       <Interests />
